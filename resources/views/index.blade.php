bc0c5c5b3b15c8e85720a4a6c668755732a43d73
5dam <ralphcadelina5@gmail.com>
2023-01-25 09:59:27 +0800

    test

---
+++ resources/views/index.blade.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @include('cdn')
+    <title>Kokusai Daigaku</title>
+</head>
+<body>
+    <!-- Header -->
+    <header class="container-fluid mt-3 ms-4">
+        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+            <a href="#" class="col-md-1 d-flex align-items-center mb-2 mb-lg-0 text-white text-deocoration-none">
+                <img src="kokusai_logo.png">
+            </a>
+            <div class="col-md-10">
+                <h1 class=""><span style="color:#F6C400;">M</span>indanao <span style="color:#F6C400;">K</span>okusai <span style="color:#F6C400;">D</span>aigaku</h1>
+                <h6 class="">THE NO. 1 JAPANESE LANGUAGE COLLEGE IN THE PHILIPPINES</h6>
+            </div>
+            <div class="col-md-1">
+                <button type="button" class="btn text-white" style="background-color:#393280;">Login</button>
+            </div>
+            <div class="col-md-12 d-flex justify-content-end mt-n3">
+                <div class="me-5">
+                    <small><i class="fa-solid fa-envelope"></i>
+                    <span>EMAIL US AT</span>
+                    <p>info@mkdph.com</p></small>
+                </div>
+                <div class="me-5">
+                    <small><i class="fa-solid fa-phone"></i>
+                    <span>CALL US ON</span>
+                    <p>+63822330013</p></small>
+                </div>
+                <div class="me-5">
+                    <small><i class="fa-solid fa-location-pin"></i>
+                    <span>ANGLIONGTO AVENUE, MAMAY ROAD, LANANG</span>
+                    <p>DAVAO CITY, DAVAO DEL SUR, PHILIPPINES</p></small>
+                </div>
+            </div>
+        </div>
+    </header>
+    <hr class="mt-n2 hr">
+
+    <!-- Navigation Menu (not responsive) -->
+   <div class="col d-flex justify-content-center text-center">
+    <h6 class="col-md-2">HOME</h6>
+    <div class="vr border"></div>
+    <h6 class="col-md-2">ACADEMICS</h6>
+    <div class="vr border"></div>
+    <h6 class="col-md-2">ADMISSION</h6>
+    <div class="vr border"></div>
+    <h6 class="col-md-2">STUDENT SERVICES</h6>
+    <div class="vr border"></div>
+    <h6 class="col-md-2">ABOUT US</h6>
+    <div class="vr border"></div>
+    <h6 class="col-md-2">CONTACT</h6>
+    <div class="vr border"></div>
+   </div>
+
+   <!-- Landing Page Heading -->
+
+
+
+ 
+
+
+
+   <!-- Mission, Philosophy, Vision -->
+    <div class="container mt-5 text-center text-dark">
+        <div class="row">
+            <div class="col-md-4">
+                <h3>MISSION</h3>
+                <p class="fst-italic" style="text-align:justify; text-justify:interword;"><span class="h2">W</span>e aim to provide a diverse range of 
+                learning opportunities and activities in
+                Japanese, English, and Filipino
+                languages, as well as other
+                competencies. Our goal is to produce
+                quality and relevant research outputs,
+                while also promoting social justice and
+                peace through exposure to diverse 
+                cultures. Additionally, we strive to
+                instill a culture of morality, civic
+                consciousness, gender sensitivity, and
+                corporate social responsibility in all of 
+                our endeavors.</p>
+            </div>
+            <div class="col-md-4">
+                <h3>PHILOSOPHY</h3>
+                <p class="fst-italic" style="text-align:justify; text-justify:interword;"><span class="h2">T</span>he College is dedicated to providing
+                a holistic education, and aims to
+                produce graduates who are well-rounded
+                individuals. Our goal is to 
+                empower students to become active
+                and engaged members of society,
+                equipped to face any global problem.
+                To achieve this, we provide quality
+                education and a wide range of learning
+                opportunities. We also focus on
+                developing the whole person, including
+                their emotional, social, and ethical 
+                abilities, in order to create graduates who have the knowledge, skills, and
+                values to make a positive impact on 
+                the world.</p>
+            </div>
+            <div class="col-md-4">
+                <h3>VISION</h3>
+                <p class="fst-italic" style="text-align:justify; text-justify:interword;"><span class="h2">A</span>n academic community proficient in
+                Japanese Language, equipped with 
+                multicultural sensitivity, strong work
+                ethics, and global competencies is 
+                essential in today's globalized world. 
+                They are well-prepared for the 
+                challenges and opportunities of the 
+                21st century, and will be able to make
+                a positive impact in their fields and in
+                the world at large.</p>
+            </div>
+        </div>
+    </div>
+
+    
+    <!-- Section 2 -->
+    <div class="" 
+     style="background: url('/images/2ndpicx.jpg');
+            height: 50vh">
+    </div>
+
+    <!-- Section 3 -->
+   <div>
+        <h1 style="
+        color:#393280;
+        width: 100%; 
+        text-align: center; 
+        border-bottom: 1px solid #F6C400; 
+        line-height: 0.1em;
+        margin: 10px 0 20px;
+        ">
+        <span style=" 
+        background:#fff; 
+        padding:0 80px;">
+        ACADEMICS
+        </span>
+    </h1>
+   </div>
+   <div class="container text-center col-md-4 mt-5">
+    <p class="fst-italic" style="text-align:justify; text-justify:interword;">"MKD desires to provide future teachers a rewarding 
+        learning experience and get maximum benefits from their
+        experiential learning courses with the end in view of 
+        transferring quality basic education for all Filipino 
+        children, which is the country's ultimate goal."
+    </p>
+    <p class="text-end"><strong>Laura V. Cespon, LPT, Ed. D.</strong> | Education Department Head</p>
+   </div>
+   <div class="container">
+        <div class="row">
+            <div class="col-sm">
+                <img src="academics_2.png" alt="test">
+            </div>
+            <div class="col-sm">
+            One of three columns
+            </div>
+            <div class="col-sm">
+            One of three columns
+            </div>
+            <div class="col-sm">
+            One of three columns
+            </div>
+            <div class="col-sm">
+            One of three columns
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+   <!-- Footer -->
+   <footer class="text-center text-white" style="background-color:#393280;">
+    <img class="mt-3" src="kokusai_logo.png">
+    <p class="mt-3 h3">The No. 1 Japanese Language College in the Philippines</p>
+    <div class="d-flex justify-content-center gap-5">
+        <small><i class="fa-solid fa-envelope"></i>
+        <span>EMAIL US AT</span>
+        <p>info@mkdph.com</p></small>
+        <small><i class="fa-solid fa-phone"></i>
+        <span>CALL US ON</span>
+        <p>+63822330013</p></small>
+        <small><i class="fa-solid fa-location-pin"></i>
+        <span>ANGLIONGTO AVENUE, MAMAY ROAD, LANANG</span>
+        <p>DAVAO CITY, DAVAO DEL SUR, PHILIPPINES</p></small>
+    </div>
+    <p>© 2023 Mindanao Kokusai Daigaku. All Rights Reserved</p>
+    </footer>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/test', function(){
+    return view('test');
 });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/index', [App\Http\Controllers\HomeController::class, 'index'])->name('index');
