415832e82535999f1471f95f38db41ba5d38db4a
RandomGuy0114 <clarenceperez686@gmail.com>
2023-01-30 23:59:02 +0800

    navbar dropdown change

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -1,74 +1,76 @@
 @extends('dashboard.layout')
 @section('content')
-<div class="container justify-content-center col-9 py-5 rounded-top-left-1 rounded-top-right-1">
-    <div class="d-flex row rounded-top" id="dashboardContainers" style="background-color: #eeefff">
-        <div class="col-3 text-center m-0 p-0">
-            <div>
-                <h5 class="dashboardContainer rounded-top-left-1 h-50">Date</h5>
-            </div>
-            <div class="h-50 d-flex align-items-center justify-content-center">
-                <h5 class="nomargin mt-2">Sample</h5>
-            </div>
-        </div>
-        <div class="container col-9 text-center m-0 p-0 ">
-            <h5 class="dashboardContainer rounded-top-right-1">Users in the system</h5>
-            <div class="row m-0 p-0">
-                <div class="col-6">
-                    <h5>Students</h5>
-                    <div class="h-50 d-flex align-items-center justify-content-center">
-                        <h5 class="nomargin mt-2">Sample</h5>
-                    </div>
+    <div class="container justify-content-center col-9 py-5 rounded-top-left-1 rounded-top-right-1">
+        <div class="d-flex row rounded-top" id="dashboardContainers" style="background-color: #eeefff">
+            <div class="col-3 text-center m-0 p-0">
+                <div>
+                    <h5 class="dashboardContainer rounded-top-left-1 h-50">Date</h5>
                 </div>
-                <div class="col-6 m-0">
-                    <h5>Teachers</h5>
-                    <div class="h-50 d-flex align-items-center justify-content-center">
-                        <h5 class="nomargin mt-2">Sample</h5>
-                    </div>
+                <div class="h-50 d-flex align-items-center justify-content-center">
+                    <h5 class="nomargin mt-2">Sample</h5>
                 </div>
             </div>
+            <div class="container col-9 text-center m-0 p-0 ">
+                <h5 class="dashboardContainer rounded-top-right-1">Users in the system</h5>
+                <div class="row m-0 p-0">
+                    <div class="col-6">
+                        <h5>Students</h5>
+                        <div class="h-50 d-flex align-items-center justify-content-center">
+                            <h5 class="nomargin mt-2">Sample</h5>
+                        </div>
+                    </div>
+                    <div class="col-6 m-0">
+                        <h5>Teachers</h5>
+                        <div class="h-50 d-flex align-items-center justify-content-center">
+                            <h5 class="nomargin mt-2">Sample</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+        <div class="d-flex row rounded-top pt-0"  >
+            <table class="table mt-5 border " id="dashboardContainers">
+                <thead>
+                    <tr>
+                        <th scope="col">Course</th>
+                        <th scope="col">Subjects</th>
+                        <th scope="col">Average Grade</th>
+                        <th scope="col">Sections</th>
+                        <th scope="col">Students</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">BSIT</th>
+                        <td>23</td>
+                        <td>80.20</td>
+                        <td>2</td>
+                        <td>124</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">BSIT</th>
+                        <td>23</td>
+                        <td>80.20</td>
+                        <td>2</td>
+                        <td>124</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">BSIT</th>
+                        <td>23</td>
+                        <td>80.20</td>
+                        <td>2</td>
+                        <td>124</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">BSIT</th>
+                        <td>23</td>
+                        <td>80.20</td>
+                        <td>2</td>
+                        <td>124</td>
+                    </tr>
+
+            </table>
+        </div>
+
     </div>
-
-    <table class="table mt-5 border" >
-        <thead >
-            <tr>
-                <th scope="col">Course</th>
-                <th scope="col">Subjects</th>
-                <th scope="col">Average Grade</th>
-                <th scope="col">Sections</th>
-                <th scope="col">Students</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <th scope="row">BSIT</th>
-                <td>23</td>
-                <td>80.20</td>
-                <td>2</td>
-                <td>124</td>
-            </tr>
-            <tr>
-                <th scope="row">BSIT</th>
-                <td>23</td>
-                <td>80.20</td>
-                <td>2</td>
-                <td>124</td>
-            </tr>
-            <tr>
-                <th scope="row">BSIT</th>
-                <td>23</td>
-                <td>80.20</td>
-                <td>2</td>
-                <td>124</td>
-            </tr>
-            <tr>
-                <th scope="row">BSIT</th>
-                <td>23</td>
-                <td>80.20</td>
-                <td>2</td>
-                <td>124</td>
-            </tr>
-
-    </table>
-</div>
 @endsection
--- resources/views/dashboard/sidenav.blade.php
+++ resources/views/dashboard/sidenav.blade.php
@@ -25,19 +25,37 @@
             </a>
         </li>
         <li class="nav-item my-1 hoverOfSidebar">
-            <div class="dropdown " >
-                <a class="nav-link link-light sideBarLink py-3 dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    <svg class="bi me-2" width="16" height="16">
-                        <use xlink:href="#table"></use>
-                    </svg>
-                    Account Management
-                </a>
-                <ul class="dropdown-menu w-100 p-1 " >
-                  <li><a class="dropdown-item my-2 py-2" href="{{ route('student_account_management') }}">Manage Accounts</a></li>
-                  <li><a class="dropdown-item my-2 py-2" href="{{ route('change_password_account_management') }}">Change Password Req.</a></li>
-                </ul>
-              </div>
+
+            <button class=" btn btn-toggle align-items-center rounded collapsed nav-link link-light sideBarLink py-3"
+                data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="false">
+                <svg class="bi me-2" width="16" height="16">
+                    <use xlink:href="#speedometer2"></use>
+                </svg>
+                Account Management
+            </button>
+            
         </li>
+        <div class="collapse" id="home-collapse" style="">
+             
+            <ul class=" w-100 p-1 btn-toggle-nav list-unstyled">
+                <li class="nav-item my-1 hoverOfSidebar">
+                    <a class="dropdown-item my-2 ms-4 py-2" href="{{ route('student_account_management') }}">
+                        <svg class="bi me-2" width="16" height="16">
+                            <use xlink:href="#speedometer2"></use>
+                        </svg>
+                        Manage Accounts
+                    </a>
+                </li>
+                <li class="nav-item my-1 hoverOfSidebar"> 
+                    <a class="dropdown-item my-2 ms-4 py-2" href="{{ route('change_password_account_management') }}">
+                        <svg class="bi me-2" width="16" height="16">
+                            <use xlink:href="#speedometer2"></use>
+                        </svg>
+                        Change Password Req.
+                    </a>
+                </li>
+            </ul>
+        </div>
         <li class="nav-item my-1 hoverOfSidebar">
             <a href="{{ route('pre_enrolled_students') }}" class="nav-link link-light sideBarLink py-3">
                 <svg class="bi me-2" width="16" height="16">
@@ -57,4 +75,70 @@
     </ul>
 </div>
 
+{{-- <div class="flex-shrink-0 p-3 bg-secondary" style="width: 280px;">
+    <a href="/" class="d-flex align-items-center pb-3 mb-3 link-dark text-decoration-none border-bottom">
+        <svg class="bi me-2" width="30" height="24">
+            <use xlink:href="#bootstrap"></use>
+        </svg>
+        <span class="fs-5 fw-semibold">Collapsible</span>
+    </a>
+    <ul class="list-unstyled ps-0">
+        <li class="mb-1">
+            <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse"
+                data-bs-target="#home-collapse" aria-expanded="false">
+                Home
+            </button>
+            <div class="collapse" id="home-collapse" style="">
+                <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                    <li><a href="#" class="link-dark rounded">Overview</a></li>
+                    <li><a href="#" class="link-dark rounded">Updates</a></li>
+                    <li><a href="#" class="link-dark rounded">Reports</a></li>
+                </ul>
+            </div>
+        </li>
+        <li class="mb-1">
+            <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse"
+                data-bs-target="#dashboard-collapse" aria-expanded="false">
+                Dashboard
+            </button>
+            <div class="collapse" id="dashboard-collapse" style="">
+                <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                    <li><a href="#" class="link-dark rounded">Overview</a></li>
+                    <li><a href="#" class="link-dark rounded">Weekly</a></li>
+                    <li><a href="#" class="link-dark rounded">Monthly</a></li>
+                    <li><a href="#" class="link-dark rounded">Annually</a></li>
+                </ul>
+            </div>
+        </li>
+        <li class="mb-1">
+            <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse"
+                data-bs-target="#orders-collapse" aria-expanded="false">
+                Orders
+            </button>
+            <div class="collapse" id="orders-collapse">
+                <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                    <li><a href="#" class="link-dark rounded">New</a></li>
+                    <li><a href="#" class="link-dark rounded">Processed</a></li>
+                    <li><a href="#" class="link-dark rounded">Shipped</a></li>
+                    <li><a href="#" class="link-dark rounded">Returned</a></li>
+                </ul>
+            </div>
+        </li>
+        <li class="border-top my-3"></li>
+        <li class="mb-1">
+            <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse"
+                data-bs-target="#account-collapse" aria-expanded="false">
+                Account
+            </button>
+            <div class="collapse" id="account-collapse">
+                <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                    <li><a href="#" class="link-dark rounded">New...</a></li>
+                    <li><a href="#" class="link-dark rounded">Profile</a></li>
+                    <li><a href="#" class="link-dark rounded">Settings</a></li>
+                    <li><a href="#" class="link-dark rounded">Sign out</a></li>
+                </ul>
+            </div>
+        </li>
+    </ul>
+</div> --}}
 {{-- End of Side Bar --}}
