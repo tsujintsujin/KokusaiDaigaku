26010224ac0960d72c8db70424216269eaa87dc0
RandomGuy0114 <clarenceperez686@gmail.com>
2023-01-31 00:11:14 +0800

    trying to collapse the sidebar doesnt work yet.

--- resources/views/dashboard/sidenav.blade.php
+++ resources/views/dashboard/sidenav.blade.php
@@ -1,5 +1,9 @@
 {{-- Side Bar --}}
 <div class="d-flex col-3 flex-column flex-shrink-0 vh-100 dashboardContainer" id="sidebar" style="width: 280px;">
+    <button class="w-100 navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <i class="fas fa-bars"> </i>
+    </button>
     <div class="text-center my-5 ">
         <img src="https://mkdph.com/wp-content/uploads/2022/06/MKD-logo-93p.png" class="rounded"
             alt="Mindanao Kokusai Daigaku Logo">
@@ -7,7 +11,7 @@
         <p class="">Account ID: MKD105915</p>
     </div>
 
-    <ul class="nav  nav-pills text-light flex-column mb-auto">
+    <ul class="nav  nav-pills text-light flex-column mb-auto" id="navbarNav">
         <li class="nav-item my-1 hoverOfSidebar">
             <a href="{{ route('dashboard') }}" class="nav-link link-light sideBarLink py-3">
                 <svg class="bi me-2" width="16" height="16">
@@ -33,10 +37,10 @@
                 </svg>
                 Account Management
             </button>
-            
+
         </li>
         <div class="collapse" id="home-collapse" style="">
-             
+
             <ul class=" w-100 p-1 btn-toggle-nav list-unstyled">
                 <li class="nav-item my-1 hoverOfSidebar">
                     <a class="dropdown-item my-2 ms-4 py-2" href="{{ route('student_account_management') }}">
@@ -46,7 +50,7 @@
                         Manage Accounts
                     </a>
                 </li>
-                <li class="nav-item my-1 hoverOfSidebar"> 
+                <li class="nav-item my-1 hoverOfSidebar">
                     <a class="dropdown-item my-2 ms-4 py-2" href="{{ route('change_password_account_management') }}">
                         <svg class="bi me-2" width="16" height="16">
                             <use xlink:href="#speedometer2"></use>
@@ -75,70 +79,5 @@
     </ul>
 </div>
 
-{{-- <div class="flex-shrink-0 p-3 bg-secondary" style="width: 280px;">
-    <a href="/" class="d-flex align-items-center pb-3 mb-3 link-dark text-decoration-none border-bottom">
-        <svg class="bi me-2" width="30" height="24">
-            <use xlink:href="#bootstrap"></use>
-        </svg>
-        <span class="fs-5 fw-semibold">Collapsible</span>
-    </a>
-    <ul class="list-unstyled ps-0">
-        <li class="mb-1">
-            <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse"
-                data-bs-target="#home-collapse" aria-expanded="false">
-                Home
-            </button>
-            <div class="collapse" id="home-collapse" style="">
-                <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                    <li><a href="#" class="link-dark rounded">Overview</a></li>
-                    <li><a href="#" class="link-dark rounded">Updates</a></li>
-                    <li><a href="#" class="link-dark rounded">Reports</a></li>
-                </ul>
-            </div>
-        </li>
-        <li class="mb-1">
-            <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse"
-                data-bs-target="#dashboard-collapse" aria-expanded="false">
-                Dashboard
-            </button>
-            <div class="collapse" id="dashboard-collapse" style="">
-                <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                    <li><a href="#" class="link-dark rounded">Overview</a></li>
-                    <li><a href="#" class="link-dark rounded">Weekly</a></li>
-                    <li><a href="#" class="link-dark rounded">Monthly</a></li>
-                    <li><a href="#" class="link-dark rounded">Annually</a></li>
-                </ul>
-            </div>
-        </li>
-        <li class="mb-1">
-            <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse"
-                data-bs-target="#orders-collapse" aria-expanded="false">
-                Orders
-            </button>
-            <div class="collapse" id="orders-collapse">
-                <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                    <li><a href="#" class="link-dark rounded">New</a></li>
-                    <li><a href="#" class="link-dark rounded">Processed</a></li>
-                    <li><a href="#" class="link-dark rounded">Shipped</a></li>
-                    <li><a href="#" class="link-dark rounded">Returned</a></li>
-                </ul>
-            </div>
-        </li>
-        <li class="border-top my-3"></li>
-        <li class="mb-1">
-            <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse"
-                data-bs-target="#account-collapse" aria-expanded="false">
-                Account
-            </button>
-            <div class="collapse" id="account-collapse">
-                <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                    <li><a href="#" class="link-dark rounded">New...</a></li>
-                    <li><a href="#" class="link-dark rounded">Profile</a></li>
-                    <li><a href="#" class="link-dark rounded">Settings</a></li>
-                    <li><a href="#" class="link-dark rounded">Sign out</a></li>
-                </ul>
-            </div>
-        </li>
-    </ul>
-</div> --}}
+
 {{-- End of Side Bar --}}
