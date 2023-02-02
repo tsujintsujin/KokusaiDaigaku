d206a4e5b2729cd20e2c4e0640ce3d3099301932
RandomGuy0114 <clarenceperez686@gmail.com>
2023-02-03 00:18:40 +0800

    teacher model -mcr

---
+++ database/migrations/2023_02_02_160656_create_subjects_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subjects', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('course_id')->references('id')->on('courses')->onDelete('cascade');
+            $table->string('subject_code');
+            $table->text('description');
+            $table->integer('units');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subjects');
+    }
+};
