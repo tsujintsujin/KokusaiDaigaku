c566176e5c16e394b7355b90359e491c72473034
5dam <ralphcadelina5@gmail.com>
2023-02-06 20:38:33 +0800

    added main.css

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ URL::asset('css/main.css') }}">
     @include('cdn')
     <title>Kokusai Daigaku</title>
 </head>
