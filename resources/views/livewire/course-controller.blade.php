34f5436190a14e12b5402b401e1c1a2ec6c0343a
RandomGuy0114 <clarenceperez686@gmail.com>
2023-02-06 13:46:50 +0800

    course table editing

--- resources/views/livewire/course-controller.blade.php
+++ resources/views/livewire/course-controller.blade.php
@@ -3,49 +3,54 @@
         <div class="row dashboardTitleContainer ps-4 rounded-top">
             <h3 class="fw-bold pt-2">Courses</h3>
         </div>
-        <div class="row dashboardContainer ps-4 pb-4">
-            <div class="row mt-4">
-                <div class="col-6">
-                    <div class="form-outline bg-white rounded p-1">
-                        <input type="text" id="" class="form-control" name="name" />
-                        <label class="form-label" for="">Course Name (Full)</label>
-                    </div>
-                </div>
-                <div class="col-2">
-                    <div class="form-outline bg-white rounded p-1">
-                        <input type="text" id="" class="form-control" name="abbr" />
-                        <label class="form-label" for="">Abbreviation</label>
-                    </div>
-                </div>
-                {{-- <form action="{{ route('createcourse') }}" method="POST"> --}}
-                {{-- @csrf --}}
+        <form action="{{ route('createcourse') }}" method="POST">
+            @csrf
+            <div class="row dashboardContainer ps-4 pb-4">
                 <div class="row mt-4">
-                    <div class="col">
-                        <div class="form-outline mb-4 bg-white rounded ">
-                            <textarea class="text-area-no-resize form-control " id="" cols="1" max-rows="4"></textarea>
-                            <label class="form-label" for="3">Description</label>
+                    <div class="col-6">
+                        <div class="form-outline bg-white rounded p-1">
+                            <input type="text" id="" class="form-control" name="name" />
+                            <label class="form-label" for="">Course Name (Full)</label>
                         </div>
                     </div>
-                </div>
-                <div class="row d-flex justify-content-end">
-                    <button class="p-2 me-3 col-1 btn btn-primary ">
-                        Add
-                    </button>
-                    <button wire:click="edit" class="p-2 me-3 col-1 btn btn-primary ">
-                        Edit
-                    </button>
-                </div>
-                {{-- </form> --}}
-            </div>
-        </div>
+                    <div class="col-2">
+                        <div class="form-outline bg-white rounded p-1">
+                            <input type="text" id="" class="form-control" name="abbreviation" />
+                            <label class="form-label" for="">Abbreviation</label>
+                        </div>
+                    </div>
 
+                    <div class="row mt-4">
+                        <div class="col">
+                            <div class="form-outline mb-4 bg-white rounded ">
+                                <textarea class="text-area-no-resize form-control " id="" cols="1" max-rows="4" name="description"></textarea>
+                                <label class="form-label" for="3">Description</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row d-flex justify-content-end">
+                        <button class="p-2 me-3 col-1 btn btn-primary " type="submit">
+                            Add
+                        </button>
+                        <button wire:click="edit" class="p-2 me-3 col-1 btn btn-primary ">
+                            Edit
+                        </button>
+                    </div>
+
+                </div>
+            </div>
+        </form>
         <div class="ps-4 mt-3">
             <div class="col-4">
                 <div class="dropdown"><label class="form-label" for="">View</label>
                     <select class="text-start form-select p-2">
-                        <option value="1">Bachelor in sadasdsa</option>
+                        {{-- <option value="1">Bachelor in sadasdsa</option>
                         <option value="2">2</option>
-                        <option value="3">3</option>
+                        <option value="3">3</option> --}}
+                        {{-- @foreach ($courses as $course)
+                            <option ><a class="dropdown-item" href="#">{{ $course->name }}</a>
+                            </option>
+                        @endforeach --}}
                     </select>
                 </div>
             </div>
@@ -150,31 +155,31 @@
                     </div>
                 </div>
             </div>
-            
 
-                <h2 class="fw-bold mt-5">Update Course to:</h2>
+
+            <h2 class="fw-bold mt-5">Update Course to:</h2>
+            <div class="row mt-4">
+                <div class="col-6">
+                    <div class="form-outline bg-white rounded p-1">
+                        <input type="text" id="" class="form-control" name="name" />
+                        <label class="form-label" for="">Course Name (Full)</label>
+                    </div>
+                </div>
+                <div class="col-2">
+                    <div class="form-outline bg-white rounded p-1">
+                        <input type="text" id="" class="form-control" name="abbr" />
+                        <label class="form-label" for="">Abbreviation</label>
+                    </div>
+                </div>
+                {{-- <form action="{{ route('createcourse') }}" method="POST"> --}}
+                {{-- @csrf --}}
                 <div class="row mt-4">
-                    <div class="col-6">
-                        <div class="form-outline bg-white rounded p-1">
-                            <input type="text" id="" class="form-control" name="name" />
-                            <label class="form-label" for="">Course Name (Full)</label>
+                    <div class="col">
+                        <div class="form-outline mb-4 bg-white rounded ">
+                            <textarea class="text-area-no-resize form-control " id=""></textarea>
+                            <label class="form-label" for="3">Description</label>
                         </div>
                     </div>
-                    <div class="col-2">
-                        <div class="form-outline bg-white rounded p-1">
-                            <input type="text" id="" class="form-control" name="abbr" />
-                            <label class="form-label" for="">Abbreviation</label>
-                        </div>
-                    </div>
-                    {{-- <form action="{{ route('createcourse') }}" method="POST"> --}}
-                    {{-- @csrf --}}
-                    <div class="row mt-4">
-                        <div class="col">
-                            <div class="form-outline mb-4 bg-white rounded ">
-                                <textarea class="text-area-no-resize form-control " id=""></textarea>
-                                <label class="form-label" for="3">Description</label>
-                            </div>
-                        </div>
                     <div class="row d-flex justify-content-end">
                         <button wire:click="create" class="p-2 me-3 col-1 btn btn-primary ">
                             Cancel
@@ -184,11 +189,11 @@
                         </button>
                     </div>
                     {{-- </form> --}}
+                </div>
+
+
+
+
             </div>
-
-
-
-            
-        </div>
     @endif
 </div>
