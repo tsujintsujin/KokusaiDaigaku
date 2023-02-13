bb4c19668afaa5e7fff79b1712f3b10eddd12bcc
tsujintsujin <117808080+tsujintsujin@users.noreply.github.com>
2023-02-13 16:27:21 +0800

    filters done, going for crud

--- app/Http/Livewire/AssignSubjectController.php
+++ app/Http/Livewire/AssignSubjectController.php
@@ -6,27 +6,33 @@ use Livewire\Component;
 use App\Models\Course;
 use App\Models\Section;
 use App\Models\Student;
+use App\Models\Subject;
 use Illuminate\Support\Arr;
 
 
 class AssignSubjectController extends Component
 {
-
-
+    public $schoolYear;
+    public $studentList = [];
+    public $subjectList = [];
+    public $yearSelection =0;
+    public $fromCourse =0;
     public $selectedCourse = NULL;
     public $selectedSection = NULL;
-    public $fromCourse = "";
     public $subjectsSelected = [];
     public $studentsSelected = [];
-    public $counter = 0;
+    public $studentSearch;
+    public $subjectSearch;
 
     public function mount(){
         $this->Section = Section::all();
         $this->Student = Student::all();
+        $this->Subject = Subject::all();
+        $this->Course = Course::all();
     }
 
 
-    public function subjectAdd($sub, $id){
+    public function subjectAdd($id, $sub){
         if (isset($this->subjectsSelected[$id])) {
             unset($this->subjectsSelected[$id]);
         }else{
@@ -40,27 +46,28 @@ class AssignSubjectController extends Component
         }else{
             $this->studentsSelected[$id] = $name;
         }
+    
     }
 
-    public function showCont(){
-        dump($this->studentsSelected, $this->subjectsSelected);
-    }
 
+    
     public function render()
-    {
-
-
-        return view('livewire.assign-subject-controller')
-        ->with('Course', Course::all())
-        ->with('Section', Section::all())
-        ->with('Student', Student::all());
+       {
+        return view('livewire.assign-subject-controller');
     }
+
+
+
 
 
     public function updatedSelectedCourse($course)
     {
         $this->selectedCourse = $course;
+        $this->studentList = [];
+        $this->subjectList = [];
         $this->selectedSection = 0;
+    $this->studentSearch="";
+
         if ($course == 0 && $this->selectedSection == 0) {
             $this->Section = Section::all();
             $this->Student = Student::all();
@@ -77,26 +84,22 @@ class AssignSubjectController extends Component
             $this->Student = Student::where('course_id', $this->selectedCourse)->get();
         }
 
-
-
         if($course==0){
             $this->selectedSection = 0;
             $this->Section = Section::all();
             $this->Student = Student::all();
         }
-
         $this->fromCourse = $course;
-        
+        $this->subjectsSelected = [];
+        $this->studentsSelected = [];
     }
 
-
-
    
-
     public function updatedSelectedSection($section)
     {
+        $this->subjectList = [];
+        $this->studentList = [];
         $this->selectedSection = $section;
-
         if ($section == 0 && $this->selectedCourse == 0) {
             $this->Student = Student::all();
         }else if($section == 0 && $this->selectedCourse != 0) {
@@ -108,5 +111,82 @@ class AssignSubjectController extends Component
         }else{
             $this->Student = Student::where('section_id', $this->selectedSection)->get();
         }
+
+        $this->subjectsSelected = [];
+        $this->studentsSelected = [];
+        $this->studentSearch="";
+
+    }
+
+
+
+public function updatedfromCourse($fromCourse){
+    $this->subjectList = [];
+    $this->subjectsSelected = [];
+    if($this->fromCourse == 0 && $this->yearSelection == 0){
+        $this->Subject = Subject::all();
+    }else if($this->fromCourse!=0 && $this->yearSelection==0){
+        $this->Subject = Subject::where('course_id', $this->fromCourse)->get();
+    }else if($this->fromCourse==0 && $this->yearSelection!=0){
+        $this->Subject = Subject::where('year_level', $this->yearSelection)->get();
+    }else{
+        $this->Subject = Subject::where(
+            [['course_id', $this->fromCourse],
+            ['year_level', $this->yearSelection]])->get();
+    }
+    $this->subjectSearch="";
+
+}
+
+public function updatedyearSelection($yearSelection){
+    $this->subjectList = [];
+    $this->subjectsSelected = [];
+    if($this->yearSelection == 0 && $this->fromCourse == 0){
+        $this->Subject = Subject::all();
+    }else if($this->yearSelection!=0 && $this->fromCourse==0){
+        $this->Subject = Subject::where('year_level', $this->yearSelection)->get();
+    }else if($this->yearSelection==0 && $this->fromCourse!=0){
+        $this->Subject = Subject::where('course_id', $this->fromCourse)->get();
+    }else{
+        $this->Subject = Subject::where(
+            [['course_id', $this->fromCourse],
+            ['year_level', $this->yearSelection]])->get();
+    }
+    $this->subjectSearch="";
+}
+
+
+
+public function updatedstudentSearch(){
+    // $this->studentSearch = $studentSearch;
+    // $this->Student = Student::where('first_name', 'LIKE', '"%'.$this->studentSearch.'%"')->orWhere('middle_name', 'LIKE', '%'.$this->studentSearch.'%')->orWhere('last_name', 'LIKE', '%'.$this->studentSearch.'%')->get();
+    $this->Student = Student::where('first_name', 'LIKE', "%$this->studentSearch%")->orWhere('last_name', 'LIKE', "%$this->studentSearch%")->orWhere('middle_name', 'LIKE', "%$this->studentSearch%")->get();
+    $this->studentsSelected = [];
+    $this->selectedCourse = 0;
+    $this->yearSelection = 0;
+    $this->studentList = 0;
+    $this->fromCourse = 0;
+    $this->Subject = Subject::all();
+
+}
+
+
+public function updatedsubjectSearch(){
+    $this->Subject = Subject::where('subject_code', 'LIKE', "%$this->subjectSearch%")->orWhere('description', 'LIKE', "%$this->subjectSearch%")->get();
+    $this->yearSelection = 0;
+    $this->fromCourse = 0;
+    $this->selectedCourse = 0;
+    $this->yearSelection = 0;
+    $this->subjectList = [];
+    $this->subjectsSelected = [];
+
+}
+
+
+
+    public function showCont(){
+        dump($this->subjectsSelected, $this->studentsSelected, );
     }
 }
+
+
--- public/js/sidenav.js
+++ public/js/sidenav.js
@@ -3,34 +3,54 @@ let sideNav;
 let navCol;
 let contentM;
 let navToggleText;
+let checkAllButton;
+let uncheckAllButton;
+let checkboxes;
+
 
 addEventListener("load", (event) => { });
 
 onload = (event) => {
-    navtoggle = document.getElementById('navToggle');
-    sideNav = document.getElementById('sidebar');
-    navCol = document.getElementById('navCol');
-    contentM = document.getElementById('contentM');
-    navToggleText = document.getElementById('navToggleText');
-
-    navtoggle.addEventListener("click", (event) => {
-       
-      sidebar.classList.toggle("active");
-        navCol.classList.toggle("col-2");
-        contentM.classList.toggle("container-fluid");
-        contentM.classList.toggle("container");
-        navtoggle.classList.toggle("active");
-        
-        if (navToggleText.innerHTML === "‹") {
-            navToggleText.innerHTML = "›";
-          } else {
-            navToggleText.innerHTML = "‹";
-          }
+  navtoggle = document.getElementById('navToggle');
+  sideNav = document.getElementById('sidebar');
+  navCol = document.getElementById('navCol');
+  contentM = document.getElementById('contentM');
+  navToggleText = document.getElementById('navToggleText');
+  checkAllButton = document.getElementById('selectAllStudentsList');
+  uncheckAllButton = document.getElementById('removeAllStudentsList');
+  checkboxes = document.querySelectorAll(".studentCheckBox");
 
 
+  navtoggle.addEventListener("click", (event) => {
+    sidebar.classList.toggle("active");
+    navCol.classList.toggle("col-2");
+    contentM.classList.toggle("container-fluid");
+    contentM.classList.toggle("container");
+    navtoggle.classList.toggle("active");
+    if (navToggleText.innerHTML === "‹") {
+      navToggleText.innerHTML = "›";
+    } else {
+      navToggleText.innerHTML = "‹";
+    }
+  });
+
+
+  checkAllButton.addEventListener("click", function () {
+    checkboxes.forEach(checkbox => {
+      checkbox.click();
     });
+  });
 
+  uncheckAllButton.addEventListener("click", function () {
+    checkboxes.forEach(checkbox => {
+      if (checkbox.checked) {
+        checkbox.click();
+      }
+    });
+  });
 };
+
+
 
 
 
