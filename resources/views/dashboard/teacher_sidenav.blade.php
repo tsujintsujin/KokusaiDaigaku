5af671b68ead7a171241e084e171446b7a783bc4
5dam <ralphcadelina5@gmail.com>
2023-02-14 15:22:53 +0800

    added dashboard icons for students and teachers

--- resources/views/dashboard/teacher_sidenav.blade.php
+++ resources/views/dashboard/teacher_sidenav.blade.php
@@ -6,16 +6,22 @@
     
     <div id="navToggle" class="text-center bg-secondary-color p-1 rounded-circle"><h3 id="navToggleText" class="text-dark">‹</h3></div>
     {{-- dashboardTitleContainer --}}
-    <div id="navToggle2" class="text-center bg-secondary-color p-1 rounded-circle">
-        <a role="button" href="{{ route('teacher') }}" id="navToggleText2" class="text-dark pt-2 btn rounded-circle btnToggle">D</a>
+    <div id="navToggle2" class="text-center bg-secondary-color pt-1 rounded-circle">
+        <a role="button" href="{{ route('teacher') }}" id="navToggleText2" class="text-dark pt-2 btn rounded-circle btnToggle">
+            <img class="vri-icon" src="{{ asset('image/icon-dashboard.png') }}" alt="">
+        </a>
     </div>
     
     
-    <div id="navToggle3" class="text-center bg-secondary-color p-1 rounded-circle">
-        <a role="button" href="{{ route('gradeinput') }}" id="navToggleText3" class="text-dark pt-2 btn rounded-circle btnToggle">G</a>
+    <div id="navToggle3" class="text-center bg-secondary-color pt-1 rounded-circle">
+        <a role="button" href="{{ route('gradeinput') }}" id="navToggleText3" class="text-dark pt-2 btn rounded-circle btnToggle">
+            <img class="vri-icon" src="{{ asset('image/icon-grade-input.png') }}" alt="">
+        </a>
     </div>
-    <div id="navToggle4" class="text-center bg-secondary-color p-1 rounded-circle">
-        <a role="button" href="{{ route('teacherchangepassword') }}" id="navToggleText4" class="text-dark pt-2 btn rounded-circle btnToggle">A</a>
+    <div id="navToggle4" class="text-center bg-secondary-color pt-1 rounded-circle">
+        <a role="button" href="{{ route('teacherchangepassword') }}" id="navToggleText4" class="text-dark pt-2 btn rounded-circle btnToggle">
+            <img class="vri-icon" src="{{ asset('image/icon-user.png') }}" alt="">
+        </a>
     </div>
     <div id="navToggle5" class="text-center bg-secondary-color p-1 rounded-circle" style="display: none">
     </div>
@@ -29,25 +35,22 @@
     <ul class="nav  text-light flex-column mb-auto">
         <li class="nav-item my-1 hoverOfSidebar">
             <a href="{{ route('teacher') }}" class="nav-link link-light sideBarLink py-3">
-                <svg class="bi me-2" width="16" height="16">
                     <use xlink:href="#home"></use>
-                </svg>
+                    <img class="vri-icon me-2 ms-4" src="{{ asset('image/icon-dashboard.png') }}" alt="">
                 Dashboard
             </a>
         </li>
         <li class="nav-item my-1 hoverOfSidebar">
-            <a href="{{ route('gradeinput') }}" class="nav-link link-light sideBarLink py-3">
-                <svg class="bi me-2" width="16" height="16">
+            <a href="{{ route('gradeinput') }}" class="nav-link link-light sideBarLink py-3">              
                     <use xlink:href="#speedometer2"></use>
-                </svg>
+                    <img class="vri-icon me-2 ms-4" src="{{ asset('image/icon-grade-input.png') }}" alt="">
                 Grade Input
             </a>
         </li>
         <li class="nav-item my-1 hoverOfSidebar">
-            <a href="{{ route('teacherchangepassword') }}" class="nav-link link-light sideBarLink py-3">
-                <svg class="bi me-2" width="16" height="16">
+            <a href="{{ route('teacherchangepassword') }}" class="nav-link link-light sideBarLink py-3">               
                     <use xlink:href="#speedometer2"></use>
-                </svg>
+                    <img class="vri-icon me-2 ms-4" src="{{ asset('image/icon-user.png') }}" alt="">
                 Account
             </a>
         </li>
