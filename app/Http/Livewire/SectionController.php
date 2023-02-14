f3644d3d2cfb3335ae631cb16b7a121f9d9d1ae1
RandomGuy0114 <clarenceperez686@gmail.com>
2023-02-15 02:03:58 +0800

    section update edit

--- app/Http/Livewire/SectionController.php
+++ app/Http/Livewire/SectionController.php
@@ -40,6 +40,12 @@ class SectionController extends Component
         $this->resetValidation('name');
     }
 
+
+    public function clearErrors()
+    {
+        $this->courseErrorClear();
+        $this->sectionErrorClear();
+    }
     // adding a data in section table
     public function create()
     {
@@ -60,61 +66,84 @@ class SectionController extends Component
     }
 
     // this function is when admin clicked the cancel button when admin is in the edit form.
-    public function create_tab()
+    public function cancel()
     {
         $this->window = "create";
-      
+        $this->reset('name', 'course_id', 'selectedCourse', 'selectedSection');
     }
 
     public function edit()
     {
         $this->window = "edit";
+        $this->reset('name', 'course_id', 'selectedCourse', 'selectedSection');
+
+        $this->clearErrors();
     }
 
- 
-    // this is for dropdown selected course.
 
-   
+    // this is for dropdown selected course.
+    public function updatingSelectedSection($section)
+    {
+        if ($this->window == 'edit') {
+            $section_data = Section::find($section);
+            $this->name = $section_data->name;
+            $this->selectedCourse = $section_data->course_id;
+            $this->selectedSection = $section;
+        }
+    }
+
     public function updatedSelectedCourse($course)
     {
         if ($course == 0 && $this->selectedSection == 0) {
             $this->Section = Section::all();
             $this->Student = Student::all();
-        }else if($course == 0 && $this->selectedSection != 0){
+        } else if ($course == 0 && $this->selectedSection != 0) {
             $this->Section = Section::where('course_id', $this->selectedCourse)->get();
             $this->Student = Student::where('section_id', $this->selectedSection)->get();
-        }else if($course != 0 && $this->selectedSection != 0){    
+        } else if ($course != 0 && $this->selectedSection != 0) {
             $this->Section = Section::where('course_id', $this->selectedCourse)->get();
             $this->Student = Student::where(
-                [['course_id', $this->selectedCourse],
-                ['section_id', $this->selectedSection]])->get();
-        }else{
+                [
+                    ['course_id', $this->selectedCourse],
+                    ['section_id', $this->selectedSection]
+                ]
+            )->get();
+        } else {
             $this->Section = Section::where('course_id', $this->selectedCourse)->get();
             $this->Student = Student::where('course_id', $this->selectedCourse)->get();
         }
 
-        if($course==0){
+        if ($course == 0) {
             $this->selectedSection = 0;
             $this->Section = Section::all();
             $this->Student = Student::all();
         }
-
     }
 
 
-
+    public function update()
+    {
+        $section = Section::find($this->selectedSection);
+        $section->course_id = $this->selectedCourse;
+        $section->name = $this->name;
+        $section->save();
+        $this->reset('selectedSection', 'selectedCourse', 'name');
+    }
     public function updatedSelectedSection($section)
     {
         $this->selectedSection = $section;
         if ($section == 0 && $this->selectedCourse == 0) {
             $this->Student = Student::all();
-        }else if($section == 0 && $this->selectedCourse != 0) {
+        } else if ($section == 0 && $this->selectedCourse != 0) {
             $this->Student = Student::where('course_id', $this->selectedCourse)->get();
-        }else if($section != 0 && $this->selectedCourse != 0) {
-           $this->Student = Student::where(
-                [['course_id', $this->selectedCourse],
-                ['section_id', $this->selectedSection]])->get();
-        }else{
+        } else if ($section != 0 && $this->selectedCourse != 0) {
+            $this->Student = Student::where(
+                [
+                    ['course_id', $this->selectedCourse],
+                    ['section_id', $this->selectedSection]
+                ]
+            )->get();
+        } else {
             $this->Student = Student::where('section_id', $this->selectedSection)->get();
         }
     }
@@ -145,8 +174,8 @@ class SectionController extends Component
     public function render()
     {
         return view('livewire.section-controller')
-        ->with('Course', Course::all())
-        ->with('Student', Student::all())
-        ->with('Section', Section::all());
+            ->with('Course', Course::all())
+            ->with('Student', Student::all())
+            ->with('Section', Section::all());
     }
 }
--- resources/views/livewire/section-controller.blade.php
+++ resources/views/livewire/section-controller.blade.php
@@ -4,12 +4,12 @@
             <h3 class="fw-bold pt-2">Sections</h3>
         </div>
         <div class="row dashboardContainer ps-4 pb-4">
+            <form wire:submit.prevent="create">
+                <div class="row mt-4">
 
-            <div class="row mt-4">
+                    <div class="col-3">
+                        <label class="form-label ps-1" for="">Select Course</label>
 
-                <div class="col-3">
-                    <label class="form-label ps-1" for="">Select Course</label>
-                    <form wire:submit.prevent="create">
                         <select wire:model="course_id" id="selectedCourse" class="text-start form-select p-2 pb-2">
                             <option value="0" selected>Course</option>
                             @foreach ($Course as $course)
@@ -23,19 +23,20 @@
                         @error('course_id')
                             <span class="error" style="color:red;">{{ $message }}</span>
                         @enderror
-                </div>
-                <div class="col-2">
-                    <label class="form-label ps-1" for="">Section Name</label>
-                    <div class="form-outline bg-white rounded p-1">
-                        <input wire:model="name" type="text" id="" class="form-control" name="name" />
                     </div>
-                    @error('name')
-                        <span class="error" style="color:red;">{{ $message }}</span>
-                    @enderror
-                </div>
+                    <div class="col-2">
+                        <label class="form-label ps-1" for="">Section Name</label>
+                        <div class="form-outline bg-white rounded p-1">
+                            <input wire:model="name" type="text" id="" class="form-control"
+                                name="name" />
+                        </div>
+                        @error('name')
+                            <span class="error" style="color:red;">{{ $message }}</span>
+                        @enderror
+                    </div>
 
-                {{-- !!!!!!!!  commented out description of section !!!!!!!!!!!! --}}
-                {{-- <div class="row mt-4">
+                    {{-- !!!!!!!!  commented out description of section !!!!!!!!!!!! --}}
+                    {{-- <div class="row mt-4">
                     <div class="col">
                         <label class="form-label ps-1" for="">Description</label>
 
@@ -45,19 +46,21 @@
                         </div>
                     </div>
                 </div> --}}
-                {{-- !!!!!!!!  commented out description of section !!!!!!!!!!!! --}}
-                <div class="row d-flex justify-content-end">
-                    <button class="p-2 me-3 col-1 btn btn-primary " type="submit">
-                        Add
-                    </button>
-                    </form>
-                    <button wire:click="edit" class="p-2 me-3 col-1 btn btn-primary ">
-                        Edit
-                    </button>
+                    {{-- !!!!!!!!  commented out description of section !!!!!!!!!!!! --}}
+                    <div class="row d-flex justify-content-end">
+                        <button class="p-2 me-3 col-1 btn btn-primary " type="submit">
+                            Add
+                        </button>
+
+
+
+                    </div>
 
                 </div>
-
-            </div>
+            </form>
+            <button wire:click="edit" class="p-2 me-3 col-1 btn btn-primary ">
+                Edit
+            </button>
         </div>
         <div class="ps-4 mt-4">
             <div class="row">
@@ -109,13 +112,14 @@
                 <tbody>
                     @if (count($Student) > 0)
                         @foreach ($Student as $student)
-                        <tr>
-                            <th>{{$student->first_name}} {{$student->middle_name}} {{$student->last_name}}</th>
-                            <th>{{$student->contact_number}}</th>
-                            <th>{{$student->email}}</th>
-                            <th>{{$student->gender}}</th>
-                            <th>-- -- --</th>
-                        </tr>
+                            <tr>
+                                <th>{{ $student->first_name }} {{ $student->middle_name }} {{ $student->last_name }}
+                                </th>
+                                <th>{{ $student->contact_number }}</th>
+                                <th>{{ $student->email }}</th>
+                                <th>{{ $student->gender }}</th>
+                                <th>-- -- --</th>
+                            </tr>
                         @endforeach
                     @else
                         <tr>
@@ -140,80 +144,99 @@
         <div class="row dashboardContainer ps-4 pb-4">
             <div class="row mt-4">
                 <div class="col-3">
-                    <label class="form-label ps-1" for="">From Which Course</label>
+                    <label class="form-label ps-1" for="">Course @error('course_id')
+                            <span class="error" style="color:red;">{{ $message }}</span>
+                        @enderror
+                    </label>
 
                     <div class="dropdown">
-                        <select class="text-start form-select p-2" id="">
-                            <option value="0" selected disabled>Select Course</option>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
+                        <select wire:model="selectedCourse" class="text-start form-select p-2">
+                            <option value="0" selected>Course</option>
+                            @foreach ($Course as $course)
+                                <option class="option" value="{{ $course->id }}">
+                                    <a class="dropdown-item" href="#">{{ $course->name }}
+                                        ({{ $course->abbreviation }})
+                                    </a>
+                                </option>
+                            @endforeach
                         </select>
                     </div>
                 </div>
                 <div class="col-3">
-                    <div class="dropdown">
-                        <label class="form-label ps-1" for="">Select Section</label>
-                        <select class="text-start form-select p-2" id="">
-                            <option value="0" selected disabled>Section</option>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
+                    <div class="dropdown"><label class="form-label" for="">Section</label>
+                        <select wire:model="selectedSection" class="text-start form-select p-2">
+                            <option value="0" selected>Section</option>
+                            @if (@isset($Section))
+                                @foreach ($Section as $section)
+                                    <option class="option" value="{{ $section->id }}">
+                                        <a class="dropdown-item">{{ $section->name }}
+                                        </a>
+                                    </option>
+                                @endforeach
+                            @endif
                         </select>
                     </div>
                 </div>
-                <div class="row mt-4">
-                    <div class="col">
-                        <div class="form-outline mb-4 bg-white rounded ">
-                            <textarea disabled class="text-area-no-resize form-control " id="" cols="1" max-rows="4"></textarea>
-                            <label class="form-label" for="">section desription here</label>
-                        </div>
-                    </div>
-                </div>
+
             </div>
         </div>
 
         <div class="row dashboardContainer ps-4 pb-4">
             <h2 class="fw-bold mt-3">Update Section to:</h2>
-            <div class="row mt-4">
-                <div class="col-3">
-                    <div class="dropdown">
-                        <label class="form-label ps-1" for="sectioncourse">Transfer other course?</label>
-                        <select class="text-start form-select p-2 p-2" id="sectioncourse">
-                            <option value="0" selected>Do not transfer</option>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                        </select>
-                    </div>
-                </div>
-                <div class="col-2">
-                    <label class="form-label ps-1" for="">New Section Name</label>
-                    <div class="form-outline bg-white rounded p-1">
-                        <input type="text" id="" class="form-control" name="abbr" />
-
-                    </div>
-                </div>
-                {{-- <form action="{{ route('createcourse') }}" method="POST"> --}}
-                {{-- @csrf --}}
+            <form wire:submit.prevent="update">
                 <div class="row mt-4">
-                    <div class="col">
-                        <label class="form-label ps-1" for="">New Description</label>
-                        <div class="form-outline mb-4 bg-white rounded ">
-                            <textarea class="text-area-no-resize form-control " id="" cols="1" max-rows="4"></textarea>
+
+                    <div class="col-3">
+                        <div class="dropdown">
+                            <label class="form-label ps-1" for="sectioncourse">Transfer other course?</label>
+                            <select class="text-start form-select p-2 p-2" id="sectioncourse"
+                                wire:model="selectedCourse">
+                                <option value="0" selected>
+                                    <a class="dropdown-item" href="#">Course
+                                    </a>
+                                </option> 
+                                @foreach ($Course as $course)
+                                    @if ($course->id != $selectedCourse)
+                                        <option class="option" value="{{ $course->id }}">
+                                            <a class="dropdown-item" href="#">{{ $course->name }}
+                                                ({{ $course->abbreviation }})
+                                            </a>
+                                        </option>
+                                    @else{
+                                        <option value="{{ $selectedCourse }}" selected>
+                                            <a class="dropdown-item" href="#">current: {{ $course->name }}
+                                              ({{ $course->abbreviation }})
+                                            </a>
+                                        </option> 
+
+                                        }
+                                    @endif
+                                @endforeach
+                            </select>
                         </div>
                     </div>
+                    <div class="col-2">
+                        <label class="form-label ps-1" for="">New Section Name</label>
+                        <div class="form-outline bg-white rounded p-1">
+                            <input type="text" id=""  wire:model="name" value="{{ $name }}" class="form-control" />
+                            <input type="text" hidden wire:model="selectedSection" name="" id="">
+                        </div>
+                    </div>
+
+
+                    <div class="row d-flex justify-content-end">
+
+                        <button class="p-2 me-3 col-3 btn btn-primary ">
+                            Update Course Details
+                        </button>
+                    </div>
+
                 </div>
-                <div class="row d-flex justify-content-end">
-                    <button wire:click="create_tab" class="p-2 me-3 col-1 btn btn-primary ">
-                        Cancel
-                    </button>
-                    <button class="p-2 me-3 col-3 btn btn-primary ">
-                        Update Course Details
-                    </button>
-                </div>
-                {{-- </form> --}}
-            </div>
+            </form>
+
+            <button wire:click="cancel" class="p-2 me-3 col-1 btn btn-primary ">
+                Cancel
+            </button>
         </div>
     @endif
 </div>
